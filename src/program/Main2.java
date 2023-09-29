abb81780c09ebdf510cf12d5f963647e6c06da0b
mrunal1992 <mrunalvt@gmail.com>
2023-09-29 18:04:22 +0530

    new feature

---
+++ src/program/Main2.java
@@ -0,0 +1,21 @@
+package program;
+
+import java.util.Arrays;
+import java.util.List;
+
+import entity.Employee;
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		List<Employee> empl= Arrays.asList(
+				new Employee(1, "Madhu", "Nagpur", 100000),
+				new Employee(2, "Megha", "Indore", 10000),
+				new Employee(3, "Nisha", "Mumbai", 200000),
+				new Employee(4, "Vasu", "Patna", 104000)
+				
+				);
+
+	}
+
+}

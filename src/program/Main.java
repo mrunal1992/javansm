d910a32cf55cd0c592d65402a30aa8d21f73936a
mrunal1992 <mrunalvt@gmail.com>
2023-09-29 17:56:00 +0530

    by onather method

--- src/program/Main.java
+++ src/program/Main.java
@@ -1,9 +1,11 @@
 package program;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -12,7 +14,9 @@ List<String> ls= Arrays.asList("Mrunal","Neelam", "Snehal","Rashi","Jeeva","Mrun
 		
 		Set<String> hs= new HashSet<String>();
 		 ls.stream().filter(n->!hs.add(n)).forEach(System.out::println);
-
+		 
+		 List<String> dupls=ls.stream().filter(n-> Collections.frequency(ls, n)>1).collect(Collectors.toList());
+		 System.out.println(dupls);
 	}
 
 }
